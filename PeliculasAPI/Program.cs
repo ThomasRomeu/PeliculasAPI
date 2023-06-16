bafc50358c605cdc47a5e926e4f6dadb10614343
Thomas <losromeu@hotmail.com>
2023-06-16 13:59:17 -0300

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
