31dfce70ee1e71f4daa43923eea640a52be195a9
Thomas <losromeu@hotmail.com>
2023-06-16 20:48:05 -0300

    completo la configuracion para subir imagenes mediante azure

--- PeliculasAPI/Controllers/ActoresController.cs
+++ PeliculasAPI/Controllers/ActoresController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PeliculasAPI.DTOs;
 using PeliculasAPI.Entidades;
+using PeliculasAPI.Servicios;
 
 namespace PeliculasAPI.Controllers
 {
@@ -13,11 +14,14 @@ namespace PeliculasAPI.Controllers
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly string contenedor = "actores";
 
-        public ActoresController(ApplicationDbContext context, IMapper mapper)
+        public ActoresController(ApplicationDbContext context, IMapper mapper, IAlmacenadorArchivos almacenadorArchivos)
         {
             this.context = context;
             this.mapper = mapper;
+            this.almacenadorArchivos = almacenadorArchivos;
         }
 
         [HttpGet]
@@ -44,6 +48,19 @@ namespace PeliculasAPI.Controllers
         public async Task<ActionResult> Post([FromForm] ActorCreacionDTO actorCreacionDTO)
         {
             var entidad = mapper.Map<Actor>(actorCreacionDTO);
+
+            if(actorCreacionDTO.Foto != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await actorCreacionDTO.Foto.CopyToAsync(memoryStream);
+                    var contenido = memoryStream.ToArray();
+                    var extension = Path.GetExtension(actorCreacionDTO.Foto.FileName);
+                    entidad.Foto = await almacenadorArchivos.GuardarArchivo(contenido, extension, contenedor, 
+                        actorCreacionDTO.Foto.ContentType);
+                }
+            }
+
             context.Add(entidad);
             await context.SaveChangesAsync();
             var dto = mapper.Map<ActorDTO>(entidad);
@@ -54,11 +71,29 @@ namespace PeliculasAPI.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult> Put(int id, [FromForm] ActorCreacionDTO actorCreacionDTO)
         {
-            var entidad = mapper.Map<Actor>(actorCreacionDTO);
-            entidad.Id = id;
-            context.Entry(entidad).State = EntityState.Modified;
-            await context.SaveChangesAsync();
+            var actorDB = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
 
+            if (actorDB == null)
+            {
+                return NotFound();
+            }
+
+            actorDB = mapper.Map(actorCreacionDTO, actorDB);
+
+            if (actorCreacionDTO.Foto != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await actorCreacionDTO.Foto.CopyToAsync(memoryStream);
+                    var contenido = memoryStream.ToArray();
+                    var extension = Path.GetExtension(actorCreacionDTO.Foto.FileName);
+                    actorDB.Foto = await almacenadorArchivos.EditarArchivo(contenido, extension, contenedor,
+                        actorDB.Foto,
+                        actorCreacionDTO.Foto.ContentType);
+                }
+            }
+
+            await context.SaveChangesAsync();
             return NoContent();
         }
 
