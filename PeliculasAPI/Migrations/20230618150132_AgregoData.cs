219fc70f7f4d28b9c66dbf59448259a8499ecabb
Thomas <losromeu@hotmail.com>
2023-06-18 12:38:36 -0300

    creo filtros para la entidad peliculas

---
+++ PeliculasAPI/Migrations/20230618150132_AgregoData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeliculasAPI.Migrations
+{
+    public partial class AgregoData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actores",
+                columns: new[] { "Id", "FechaNacimiento", "Foto", "Nombre" },
+                values: new object[,]
+                {
+                    { 2, new DateTime(1962, 1, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Jim Carrey" },
+                    { 4, new DateTime(1965, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Robert Downey Jr." },
+                    { 6, new DateTime(1981, 6, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Chris Evans" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Generos",
+                columns: new[] { "Id", "Nombre" },
+                values: new object[,]
+                {
+                    { 4, "Aventura" },
+                    { 6, "Animación" },
+                    { 7, "Suspenso" },
+                    { 8, "Romance" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Peliculas",
+                columns: new[] { "Id", "EnCines", "FechaEstreno", "Poster", "Titulo" },
+                values: new object[,]
+                {
+                    { 2, true, new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Avengers: Endgame" },
+                    { 4, false, new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Avengers: Infinity Wars" },
+                    { 5, false, new DateTime(2020, 2, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Sonic the Hedgehog" },
+                    { 6, false, new DateTime(2020, 2, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Emma" },
+                    { 7, false, new DateTime(2020, 8, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Wonder Woman 1984" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PeliculasActores",
+                columns: new[] { "ActorId", "PeliculaId", "Orden", "Personaje" },
+                values: new object[,]
+                {
+                    { 2, 5, 1, "Dr. Ivo Robotnik" },
+                    { 4, 2, 1, "Tony Stark" },
+                    { 4, 4, 1, "Tony Stark" },
+                    { 6, 2, 2, "Steve Rogers" },
+                    { 6, 4, 2, "Steve Rogers" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PeliculasGeneros",
+                columns: new[] { "GeneroId", "PeliculaId" },
+                values: new object[,]
+                {
+                    { 4, 2 },
+                    { 4, 4 },
+                    { 4, 5 },
+                    { 4, 7 },
+                    { 7, 2 },
+                    { 7, 4 },
+                    { 7, 6 },
+                    { 7, 7 },
+                    { 8, 6 }
+                });            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PeliculasActores");
+
+            migrationBuilder.DropTable(
+                name: "PeliculasGeneros");
+
+            migrationBuilder.DropTable(
+                name: "Actores");
+
+            migrationBuilder.DropTable(
+                name: "Generos");
+
+            migrationBuilder.DropTable(
+                name: "Peliculas");
+        }
+    }
+}
